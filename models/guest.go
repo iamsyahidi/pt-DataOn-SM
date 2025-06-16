86ea52ce13e2ad323132256c3eb637565dae37e7
Ilham Syahidi <ilhamsyahidi66@gmail.com>
2025-06-16 14:10:25 +0700

    fix: define constraint

--- models/guest.go
+++ models/guest.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Guest struct {
 	gorm.Model
-	Name   string `json:"name"`
-	Email  string `json:"email"`
-	Phone  string `json:"phone"`
-	IDCard string `json:"id_card"`
-	Remark string `json:"remark"`
-	Status string `json:"status"`
+	Name   string `json:"name" gorm:"not null"`
+	Email  string `json:"email" gorm:"not null;unique"`
+	Phone  string `json:"phone" gorm:"not null"`
+	IDCard string `json:"id_card" gorm:"not null;unique"`
+	Remark string `json:"remark" gorm:"not null"`
+	Status string `json:"status" gorm:"not null;default:'active'"`
 }
